1fde4fd5bc74e6e74bd5e58f533f1c41ffa7bd76
Timur <t.fralik@aimprosoft.com>
2021-02-09 19:12:02 +0200

    Add 9 video

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,8 +4,10 @@ namespace App;
 
 use App\GildedRose\Backstage;
 use App\GildedRose\Brie;
+use App\GildedRose\Conjured;
 use App\GildedRose\Item;
 use App\GildedRose\Sulfuras;
+use InvalidArgumentException;
 
 class GildedRose
 {
@@ -14,13 +16,14 @@ class GildedRose
         'Aged Brie' => Brie::class,
         'Sulfuras, Hand of Ragnaros' => Sulfuras::class,
         'Backstage passes to a TAFKAL80ETC concert' => Backstage::class,
+        'Conjured Mana Cake' => Conjured::class
     ];
 
 
     public static function of($name, $quality, $sellIn)
     {
-        if (! array_key_exists($name, self::$items)) {
-            throw new \InvalidArgumentException('Item type does not exist.');
+        if (!array_key_exists($name, self::$items)) {
+            throw new InvalidArgumentException('Item type does not exist.');
         }
 
         $class = self::$items[$name];
